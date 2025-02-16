4fa4e375e625f8c3f1265d73d10d777c8ea59c67
antonelamatanovich <antonelamatanovic@icloud.com>
2025-02-16 13:47:41 +0100

    transform extension method

--- Assets/DialogueSystemPackage/Runtime/Scripts/ExtensionMethods/TransformExtension.cs
+++ Assets/DialogueSystemPackage/Runtime/Scripts/ExtensionMethods/TransformExtension.cs
@@ -1,6 +1,19 @@
+/********************************************************************************
+ * Script:        TransformExtension.cs
+ * Author:        Antonela Matanović
+ * Date created:  February 16th, 2025
+ * Last modified: February 16th, 2025
+ * Description:   Contains extension methods for the Transform class.
+ *                Includes a method to smoothly move a transform to a target position.
+ ********************************************************************************/
+
 using UnityEngine;
 
-public class TransformExtension
+public static class TransformExtension
 {
-    
+    // smoothly moves a transform to a target position over time
+    public static void MoveTo(this Transform transform, Vector3 target, float duration)
+    {
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime / duration);
+    }
 }
